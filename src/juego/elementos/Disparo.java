62b580eb1584e449f4b4483b9829498b8c8e7a48
usuario <usuario@samsung-mini>
2011-08-08 09:14:42 +0200

    Mejora de disparo.
    -Detecta colisiones.

--- src/juego/elementos/Disparo.java
+++ src/juego/elementos/Disparo.java
@@ -3,6 +3,9 @@ package juego.elementos;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import juego.Juego;
+import juego.motor_colisiones.MotorColisiones;
+
 import plantillas.elementos.ObjetoMovil;
 import plantillas.elementos.PuntoMovil;
 import plantillas.tipos_de_datos.EstadoObjetoMovil;
@@ -28,6 +31,11 @@ import plantillas.tipos_de_datos.EstadoObjetoMovil;
  * 
  */
 public final class Disparo extends ObjetoMovil {
+	/**
+	 * 
+	 * Constructor
+	 * 
+	 */
 	Disparo(int x_pos, int y_pos, int ancho, int alto, int velocidad_y,
 			Color color, EstadoObjetoMovil estado) {
 		super.esquina_superior_izquierda = new PuntoMovil();
@@ -41,7 +49,16 @@ public final class Disparo extends ObjetoMovil {
 		super.estado = estado;
 	}
 
+	/**
+	 * 
+	 * Metodos
+	 * 
+	 */
 	public void movimiento() {
+		/*
+		 * Movimiento del disparo comprobando colisiones con nave_jugador y
+		 * bichos.
+		 */
 		int sentido = 0;
 
 		if (super.estado == EstadoObjetoMovil.up
@@ -52,12 +69,54 @@ public final class Disparo extends ObjetoMovil {
 			sentido = 1;
 		}
 
+		if (this.colision_nave_jugador()) {
+			Juego.nave_jugador.destruir();
+		}
+
+		int i;
+		if ((i = this.colision_bicho()) > -1) {
+			Juego.bichos[i].destruir();
+			Juego.bichos[i] = null;
+		}
+
 		this.esquina_superior_izquierda.y_pos += this.esquina_superior_izquierda.velocidad_y
 				* sentido;
 	}
 
+	private boolean colision_nave_jugador() {
+		/*
+		 * Comprueba si colisiona con la nave del jugador.
+		 */
+		boolean colision = false;
+
+		if (MotorColisiones.figura_con_figura(this, Juego.nave_jugador)) {
+			colision = true;
+		}
+
+		return colision;
+	}
+
+	private int colision_bicho() {
+		/*
+		 * Comprueba si colisiona con un bicho y devuelve su indice.
+		 */
+		int colision = -1;
+
+		for (int i = 0; i < Juego.bichos.length; i++) {
+			if (Juego.bichos[i] != null
+					&& MotorColisiones.figura_con_figura(this, Juego.bichos[i])) {
+				colision = i;
+			}
+		}
+
+		return colision;
+	}
+
 	@Override
 	public void dibujar(Graphics g) {
+		/*
+		 * Dibuja el elemento segun sus propiedades.
+		 */
 		g.setColor(super.color);
 		g.fillRect(super.esquina_superior_izquierda.x_pos,
 				super.esquina_superior_izquierda.y_pos, super.ancho, super.alto);
