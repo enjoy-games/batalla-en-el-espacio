7174dc0dae7fadce3cfaee380ae51f576babc07e
usuario <usuario@samsung-mini.local.lan>
2011-08-16 13:39:45 +0200

    Arreglado error de ContadorVida.
    - Al no existir, ya no se crea instancia.

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -2,7 +2,6 @@ package juego;
 
 import plantillas.PlantillaJuego;
 import juego.elementos.Bicho;
-import juego.elementos.ContadorVida;
 import juego.elementos.Disparo;
 import juego.elementos.InteligenciaArtificial;
 import juego.elementos.NaveJugador;
@@ -37,7 +36,6 @@ public class Juego extends PlantillaJuego {
 	public int numero_de_enemigos = 40;
 	public NaveJugador nave_jugador;
 	public Disparo disparo_jugador;
-	public ContadorVida contador_vida;
 	public Bicho[] bichos = new Bicho[this.numero_de_enemigos];
 	public Disparo[] disparo_bichos = new Disparo[this.numero_de_enemigos];
 	public InteligenciaArtificial ia;
@@ -56,8 +54,6 @@ public class Juego extends PlantillaJuego {
 		// Elementos de juego.
 
 		this.nave_jugador = new NaveJugador(200 - 20, 400 - 40, 40, 40, 8, this);
-
-		this.contador_vida = new ContadorVida(10, 410, 15, 20);
 
 		// Enemigos
 		int x = 0, y = 33, ancho = 40, alto = 40;
